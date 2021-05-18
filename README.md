5eda8f0ede86650a07f71f781197ceaddffbe775
Rafael Gabriel <git@rafaelgabriel.dev>
2021-05-18 09:26:30 -0300

    added swiftenv

---
+++ README.md
@@ -0,0 +1,19 @@
+# Example
+```Shell
+curl -s -XGET https://raw.githubusercontent.com/rafaelprimeiro/myscripts/master/echo.sh | bash -s
+```
+
+## Install swift
+### For Mac
+```Shell
+curl -s -XGET https://raw.githubusercontent.com/rafaelprimeiro/myscripts/master/install_swift.sh | bash -s mac
+```
+### For Linux
+```Shell
+curl -s -XGET https://raw.githubusercontent.com/rafaelprimeiro/myscripts/master/install_swift.sh | bash -s linux
+```
+
+### Example install swiftenv with swift version 5.4 on linux
+```Shell
+curl -s -XGET https://raw.githubusercontent.com/rafaelprimeiro/myscripts/master/install_swift.sh | bash -s linux 5.4
+```
---
+++ install_swift.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+
+if [[ -d ~/.swiftenv ]]; then
+    echo "Remove old..."
+    rm -Rf ~/.swiftenv
+fi
+
+echo "Start download..."
+git clone https://github.com/kylef/swiftenv.git ~/.swiftenv
+
+echo "Configuring..."
+if [[ $1 == 'mac' ]]; then
+    echo 'export SWIFTENV_ROOT="$HOME/.swiftenv"' >> ~/.bash_profile
+    echo 'export PATH="$SWIFTENV_ROOT/bin:$PATH"' >> ~/.bash_profile
+    echo 'eval "$(swiftenv init -)"' >> ~/.bash_profile
+
+    echo 'export SWIFTENV_ROOT="$HOME/.swiftenv"' >> ~/.zshenv
+    echo 'export PATH="$SWIFTENV_ROOT/bin:$PATH"' >> ~/.zshenv
+    echo 'eval "$(swiftenv init -)"' >> ~/.zshenv
+elif [[ $1 == 'linux' ]]; then
+    echo 'export SWIFTENV_ROOT="$HOME/.swiftenv"' >> ~/.bashrc
+    echo 'export PATH="$SWIFTENV_ROOT/bin:$PATH"' >> ~/.bashrc
+    echo 'eval "$(swiftenv init -)"' >> ~/.bashrc
+else 
+    echo 'ERROR: required system <mac|linux> with first param'
+    exit 8
+fi
+
+if [[ ! -z "$2" ]]; then
+    echo "Install swift version ${2}"
+    ~/.swiftenv/bin/swiftenv install $2
+fi
+
+echo "Done."
